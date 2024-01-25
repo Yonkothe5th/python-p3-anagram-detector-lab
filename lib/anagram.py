c08c6333b2d831291fa2b4ac3004729f6e508b0d
Brian Lawi Ochiel <lawioochiel@gmail.com>
2024-01-25 14:27:33 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.letters_in_word = sorted([letter for letter in word])
+        
+    def match(self, word_list):
+        match_word_list = []
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.letters_in_word:
+                match_word_list.append(word)
+        return match_word_list
+
